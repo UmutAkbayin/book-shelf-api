f6b77284d444169f7ab6b5b6d0bbd4db0ab87ae9
Umut Akbayin <umutakbayin@posteo.de>
2024-11-09 21:36:08 +0100

    chore: initial commit

---
+++ src/main/java/dev/akbayin/bookshelfapi/BookShelfApiApplication.java
@@ -0,0 +1,13 @@
+package dev.akbayin.bookshelfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShelfApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/akbayin/bookshelfapi/BookShelfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akbayin.bookshelfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShelfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
