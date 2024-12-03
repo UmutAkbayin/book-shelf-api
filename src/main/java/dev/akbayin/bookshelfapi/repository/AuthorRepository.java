443933f40c5937e22016e3b141075d6550867143
Umut Akbayin <umutakbayin@posteo.de>
2024-12-03 19:18:36 +0100

    chore: remove unused import statements

--- src/main/java/dev/akbayin/bookshelfapi/repository/AuthorRepository.java
+++ src/main/java/dev/akbayin/bookshelfapi/repository/AuthorRepository.java
@@ -2,8 +2,6 @@ package dev.akbayin.bookshelfapi.repository;
 
 import dev.akbayin.bookshelfapi.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
