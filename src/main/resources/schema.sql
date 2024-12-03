f4ae9aeb2bd0aaca1441b9a63d18b329234b2b2b
Umut Akbayin <umutakbayin@posteo.de>
2024-12-03 17:03:29 +0100

    chore: add book_authors table for many-to-many relationship
    - Added book_authors table to represent a many-to-many relationship between books and authors.
    - Adjusted books table to remove direct author_id column.
    - Updated schema to ensure proper foreign key constraints and relationships.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,11 +16,17 @@ CREATE TABLE IF NOT EXISTS `book_shelf`.`publishers` (
 CREATE TABLE IF NOT EXISTS `book_shelf`.`books` (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(200) NOT NULL,
-    author_id INT NOT NULL,
     publisher_id INT NOT NULL,
     publishing_year YEAR NOT NULL CHECK (publishing_year >= 1450),
     status ENUM('read', 'want to read', 'reading', 'not read') NOT NULL DEFAULT 'not read',
     PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES book_shelf.authors(id) ON DELETE CASCADE,
     FOREIGN KEY (publisher_id) REFERENCES book_shelf.publishers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `book_shelf`.`book_authors` (
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES book_shelf.books(id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES book_shelf.authors(id) ON DELETE CASCADE
 );
