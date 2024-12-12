231fdd4f8e75c61dd4b4f5f93a73a338ed1230c2
Umut Akbayin <umutakbayin@posteo.de>
2024-12-12 13:26:26 +0100

    Refactor(PublisherReferenceConverter): Change field injection to constructor injection
    This change aligns with best practices for improved testability and code maintainability.

--- src/main/java/dev/akbayin/bookshelfapi/converter/PublisherReferenceConverter.java
+++ src/main/java/dev/akbayin/bookshelfapi/converter/PublisherReferenceConverter.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.util.StdConverter;
 import dev.akbayin.bookshelfapi.model.Publisher;
 import dev.akbayin.bookshelfapi.repository.PublisherRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PublisherReferenceConverter extends StdConverter<String, Publisher> {
-    @Autowired
-    private PublisherRepository publisherRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public PublisherReferenceConverter(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
 
     @Override
     public Publisher convert(String publisherPath) {
