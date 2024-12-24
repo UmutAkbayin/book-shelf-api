abdce8df6ea3501c2cdab4c7ef275e4bd2e7a499
Umut Akbayin <umutakbayin@posteo.de>
2024-12-24 15:17:42 +0100

    feat(Publisher): add @NotNull validation to Publisher model class

--- src/main/java/dev/akbayin/bookshelfapi/model/Publisher.java
+++ src/main/java/dev/akbayin/bookshelfapi/model/Publisher.java
@@ -1,10 +1,12 @@
 package dev.akbayin.bookshelfapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@NotNull
 @Table(name = "publishers")
 public class Publisher {
 
