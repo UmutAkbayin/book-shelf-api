6c77a5c69b7db39ed3f0372f470c8195c4215b12
Umut Akbayin <umutakbayin@posteo.de>
2024-12-24 14:25:34 +0100

    chore: remove AuthorRepository to restrict direct access

--- src/main/java/dev/akbayin/bookshelfapi/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.akbayin.bookshelfapi.repository;
-
-import dev.akbayin.bookshelfapi.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
