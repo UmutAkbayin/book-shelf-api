2569f8f6857dc8b85de6df734c0dbfac9ed56c82
Umut Akbayin <umutakbayin@posteo.de>
2024-12-24 14:25:57 +0100

    chore: remove PublisherRepository to restrict direct access

--- src/main/java/dev/akbayin/bookshelfapi/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.akbayin.bookshelfapi.repository;
-
-import dev.akbayin.bookshelfapi.model.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-}
