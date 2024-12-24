09787c52d7c3cd0c6e5182c4593083109fac8d01
Umut Akbayin <umutakbayin@posteo.de>
2024-12-24 17:33:25 +0100

    test: configure test database and add BookRepositoryTest class

---
+++ src/test/java/dev/akbayin/bookshelfapi/repositories/BookRepositoryTest.java
@@ -0,0 +1,9 @@
+package dev.akbayin.bookshelfapi.repositories;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class BookRepositoryTest {
+}
