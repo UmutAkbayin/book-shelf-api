b07d8f0e22a53b6b16919fba03dda6bda9eec8d8
Umut Akbayin <umutakbayin@posteo.de>
2024-12-25 20:52:00 +0100

    test(BookServiceTest): add #testCreateBook test

--- src/test/java/dev/akbayin/bookshelfapi/service/BookServiceTest.java
+++ src/test/java/dev/akbayin/bookshelfapi/service/BookServiceTest.java
@@ -1,10 +1,12 @@
 package dev.akbayin.bookshelfapi.service;
 
+import dev.akbayin.bookshelfapi.dto.BookDto;
 import dev.akbayin.bookshelfapi.model.Author;
 import dev.akbayin.bookshelfapi.model.Book;
 import dev.akbayin.bookshelfapi.model.Publisher;
 import dev.akbayin.bookshelfapi.model.Status;
 import dev.akbayin.bookshelfapi.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,16 +30,30 @@ public class BookServiceTest {
     private BookService bookService;
 
     @Test
+    @DisplayName("Create a new Book that does not exist")
     public void testCreateBook() {
-        Book book = new Book();
+        BookDto bookDto = new BookDto();
         Publisher publisher = new Publisher("Manning");
         Author author = new Author();
         author.setFirstName("Laurentiu");
         author.setLastName("Spilca");
         Set<Author> authors = new HashSet<>();
         authors.add(author);
-        book.setAuthors(authors);
-        book.setPublisher(publisher);
+        bookDto.setAuthors(authors);
+        bookDto.setPublisher(publisher);
+        bookDto.setTitle("Spring Security in Action");
+        bookDto.setPublishingYear(2024);
+        bookDto.setStatus(Status.NOT_READ);
+
+        Book book = new Book();
+        Publisher otherPublisher = new Publisher("Manning");
+        Author otherAuthor = new Author();
+        otherAuthor.setFirstName("Laurentiu");
+        otherAuthor.setLastName("Spilca");
+        Set<Author> otherAuthors = new HashSet<>();
+        otherAuthors.add(otherAuthor);
+        book.setAuthors(otherAuthors);
+        book.setPublisher(otherPublisher);
         book.setTitle("Spring Security in Action");
         book.setPublishingYear(2024);
         book.setStatus(Status.NOT_READ);
@@ -46,11 +62,11 @@ public class BookServiceTest {
 
 
 
-        bookService.createBook().ifPresent((savedBook) -> {
+        bookService.createBook(bookDto).ifPresent((savedBook) -> {
             assertNotNull(savedBook);
             assertEquals("Spring Security in Action", savedBook.getTitle());
-            assertTrue(savedBook.getAuthors().contains(author));
-            verify(bookRepository, times(1)).save(book);
+            assertTrue(savedBook.getAuthors().contains(otherAuthor));
         });
+        verify(bookRepository, times(1)).save(any(Book.class));
     }
 }
