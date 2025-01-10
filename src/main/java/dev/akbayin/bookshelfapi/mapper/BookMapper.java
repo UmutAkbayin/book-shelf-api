440dfa440c53a24ae1137291a98797fdc6e0047e
Umut Akbayin <umutakbayin@posteo.de>
2025-01-10 20:11:31 +0100

    chore: remove unused imports

--- src/main/java/dev/akbayin/bookshelfapi/mapper/BookMapper.java
+++ src/main/java/dev/akbayin/bookshelfapi/mapper/BookMapper.java
@@ -3,7 +3,6 @@ package dev.akbayin.bookshelfapi.mapper;
 import dev.akbayin.bookshelfapi.model.Book;
 import dev.akbayin.bookshelfapi.dto.BookDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = {AuthorMapper.class, PublisherMapper.class})
--- src/main/java/dev/akbayin/bookshelfapi/service/BookService.java
+++ src/main/java/dev/akbayin/bookshelfapi/service/BookService.java
@@ -11,8 +11,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class BookService {
 
